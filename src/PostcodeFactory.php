42da818a0e69428a8429140a39f952a4cc5e8af8
john.mccracken <john.mccracken@qanw.co.uk>
2017-06-12 12:27:21 +0100

    checkValidPostcode tweak preg_match check

--- src/PostcodeFactory.php
+++ src/PostcodeFactory.php
@@ -97,7 +97,7 @@ class PostcodeFactory
     protected function checkValidPostcode($postcode)
     {
         $regExp = "/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][‌​0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) ?[0-9][A-Za-z]{2})$/";
-        return (preg_match($regExp, $postcode) === false) ? false : true;
+        return (empty(preg_match($regExp, $postcode))) ? false : true;
     }
 
     /**
